21e8282e678aa2be52e168caf05e5908cf3a7614
BradyDriebergen <bradydriebergen@u.boisestate.edu>
2024-07-27 00:20:28 -0600

    Cleaned up Code/Comments

--- Main.py
+++ Main.py
@@ -2,18 +2,21 @@ from bs4 import BeautifulSoup
 import requests
 from transformers import pipeline
 
-
+# Summarizer pipeline
 summarizer = pipeline('summarization', model="sshleifer/distilbart-cnn-12-6")
 
-url_name = "https://idahonews.com/"
-url = requests.get(url_name)
-soup = BeautifulSoup(url.content, 'html.parser')
+# Website to scrape
+url = "https://idahonews.com/"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
 
 section = soup.find(class_="heroLayout-module_right__kGNn")
 
+# Loop through the first 3 articles
 for link in section.find_all('a')[1:7:2]:
-    temp_url = requests.get(url_name + link.get('href'))
-    temp_soup = BeautifulSoup(temp_url.content, 'html.parser')
+    # Get the article text
+    temp_response = requests.get(url + link.get('href'))
+    temp_soup = BeautifulSoup(temp_response.content, 'html.parser')
     temp_section = temp_soup.find(class_="StoryText-module_storyText__FWhP")
 
     url_text = ""
@@ -22,6 +25,7 @@ for link in section.find_all('a')[1:7:2]:
     
     print("Title: " + link.get_text())
     
+    # Summarize the article using AI
     summaryList = summarizer(url_text, max_length=130, min_length=30, do_sample=False)
     summary = str(summaryList[0]['summary_text']).replace(" .", ".")
     print("Summary: " + summary + '\n')
